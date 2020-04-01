4c055412d37c271c1cc1b2d4830d5dd1fdb453d2
bala <ebalakumar@yahoo.co.in>
2020-04-01 13:48:03 +0530

    first problem completed

--- adventCalendar.py
+++ adventCalendar.py
@@ -19,21 +19,21 @@ def first_problem(mass_list) -> object:
     return total_final_mass
 
 
-def second_problem(mass_list) -> object:
-    total_final_mass = 0
-    for mass in mass_list:
-        # print(mass[0])
-        int_mass = int(mass[0])
-        # final_mass = calculate_mass(int_mass)
-        # if final_mass != 0 & final_mass < 0:
-        while final_mass  != 0 & final_mass < 0:
-            
-
-
-        total_final_mass += final_mass
-
-    print(total_final_mass)
-    return total_final_mass
+# def second_problem(mass_list) -> object:
+#     total_final_mass = 0
+#     for mass in mass_list:
+#         # print(mass[0])
+#         int_mass = int(mass[0])
+#         # final_mass = calculate_mass(int_mass)
+#         # if final_mass != 0 & final_mass < 0:
+#         while final_mass  != 0 & final_mass < 0:
+#
+#
+#
+#         total_final_mass += final_mass
+#
+#     print(total_final_mass)
+#     return total_final_mass
 
 
 def main():
@@ -42,7 +42,7 @@ def main():
     with open(path) as f:
         masses = list(csv.reader(f))
     first_problem(masses)
-    second_problem(masses)
+    # second_problem(masses)
 
 
-# main()
+main()
