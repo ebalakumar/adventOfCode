09c3ff3d072681d409d5260f486ccd329589fedd
bala <ebalakumar@yahoo.co.in>
2020-04-01 16:51:04 +0530

    problem 2: opcode 1 is added.

--- adventCalendar.py
+++ adventCalendar.py
@@ -35,14 +35,34 @@ def first_problem(mass_list) -> object:
 #     print(total_final_mass)
 #     return total_final_mass
 
+def second_problem(mass_list) -> object:
+    # print(mass_list)
+    i = 0
+    test_mass_list = mass_list
+    print(opcode_calculator(i, mass_list))
+    return 0
+
+
+def opcode_calculator(i, mass_list) -> object:
+    while i < len(mass_list):
+        opcode = mass_list[i]
+        if opcode == '1':
+            mass_list[i + 3] = int(mass_list[i + 1]) + int(mass_list[i + 2])
+        i += 4
+    return mass_list
+
 
 def main():
     my_path = os.path.abspath(os.path.dirname(__file__))
-    path = os.path.join(my_path, "input.csv")
+    # path = os.path.join(my_path, "input_1.csv")
+    # with open(path) as f:
+    #     masses_1 = list(csv.reader(f))
+    # first_problem(masses_1)
+
+    path = os.path.join(my_path, "input_2.csv")
     with open(path) as f:
-        masses = list(csv.reader(f))
-    first_problem(masses)
-    # second_problem(masses)
+        masses_2 = list(csv.reader(f))
+    second_problem(masses_2[0])
 
 
 main()
