ab276826c45db31ce998aa41d64fcac719308a2b
bala <ebalakumar@yahoo.co.in>
2020-04-02 22:26:38 +0530

    problem 3:Completed part 1 of problem 3.

--- adventCalendar.py
+++ adventCalendar.py
@@ -34,6 +34,59 @@ def second_problem(mass_list) -> object:
     return mass_list
 
 
+def third_problem(red_wire, blue_wire) -> int:
+    count = 0
+    x = 0
+    y = 0
+    red_wire_path = ['0,0']
+    blue_wire_path = ['0,0']
+    update_path(red_wire, red_wire_path, x, y)
+    update_path(blue_wire, blue_wire_path, x, y)
+    # Removing initial co-ordinates
+    red_wire_path.pop(0)
+    blue_wire_path.pop(0)
+    common_coordinates = common_member(red_wire_path, blue_wire_path)
+    arr_common_coordinates = []
+    for coordinate in common_coordinates:
+        arr_common_coordinates.append(abs(int(str(coordinate).split(',')[0]))+abs(int(str(coordinate).split(',')[1])))
+    return min(arr_common_coordinates)
+
+
+def update_path(wire, wire_path, x, y):
+    for input_path in wire:
+        if 'R' == input_path[0]:
+            count = int(input_path[1:len(input_path)])
+            for value in range(count):
+                x += 1
+                wire_path.append(str(x) + ',' + str(y))
+        if 'L' == input_path[0]:
+            count = int(input_path[1:len(input_path)])
+            for value in range(count):
+                x -= 1
+                wire_path.append(str(x) + ',' + str(y))
+        if 'U' == input_path[0]:
+            count = int(input_path[1:len(input_path)])
+            for value in range(count):
+                y += 1
+                wire_path.append(str(x) + ',' + str(y))
+        if 'D' == input_path[0]:
+            count = int(input_path[1:len(input_path)])
+            for value in range(count):
+                y -= 1
+                wire_path.append(str(x) + ',' + str(y))
+
+
+def common_member(a, b) -> object:
+    common_co_ordinates = []
+    a_set = set(a)
+    b_set = set(b)
+    if a_set & b_set:
+        common_co_ordinates = list(a_set & b_set)
+    else:
+        print("No common elements")
+    return common_co_ordinates
+
+
 my_path = os.path.abspath(os.path.dirname(__file__))
 path = os.path.join(my_path, "input_1.csv")
 with open(path) as f:
@@ -44,3 +97,9 @@ path = os.path.join(my_path, "input_2.txt")
 with open(path) as f:
     masses_2 = list(map(int, list(csv.reader(f))[0]))
 print(second_problem(masses_2))
+
+path = os.path.join(my_path, "input_3.txt")
+lines = open(path).read().splitlines()
+if len(lines) > 2:
+    raise ValueError('More than two inputs are not accepted. Please check the input format')
+print(third_problem(lines[0].split(','), lines[1].split(',')))
--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 import unittest
+import os.path
 
-from adventCalendar import first_problem, second_problem
+from adventCalendar import first_problem, second_problem, third_problem
+
+my_path = os.path.abspath(os.path.dirname(__file__))
 
 
 class Test1(unittest.TestCase):
@@ -13,3 +16,17 @@ class Test1(unittest.TestCase):
 
     def test_second_Problem_2(self):
         self.assertEqual(second_problem([2, 4, 4, 5, 99, 0]), [2, 4, 4, 5, 99, 9801])
+
+    def test_third_problem_1(self):
+        path = os.path.join(my_path, "input_3_1.txt")
+        lines = open(path).read().splitlines()
+        if len(lines) > 2:
+            raise ValueError('More than two inputs are not accepted. Please check the input format')
+        self.assertEqual(third_problem(lines[0].split(','), lines[1].split(',')), 159)
+
+    def test_third_problem_2(self):
+        path = os.path.join(my_path, "input_3_2.txt")
+        lines = open(path).read().splitlines()
+        if len(lines) > 2:
+            raise ValueError('More than two inputs are not accepted. Please check the input format')
+        self.assertEqual(third_problem(lines[0].split(','), lines[1].split(',')), 135)
