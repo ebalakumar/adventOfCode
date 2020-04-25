8d039bb7f03a09b084b7eacfc5f698996e2d02dc
Balakumar Ezhilmaran <ebalakumar@yahoo.co.in>
2020-04-25 16:42:51 +0530

    problem 8, part 2: completed (Incorect answer)

--- space_image_format.py
+++ space_image_format.py
@@ -1,6 +1,6 @@
 def calculate_bios_password(wide, tall, encoded_data):
     individual_layer = []
-    total_layers = {}
+    total_layers = []
     k = 0
     while k < len(encoded_data):
         individual_layer.clear()
@@ -8,9 +8,10 @@ def calculate_bios_password(wide, tall, encoded_data):
             for i in range(wide):
                 individual_layer.append(encoded_data[k])
                 k += 1
-
-        total_layers[find_numbers(individual_layer[:], "0")] = individual_layer[:]
-    return find_numbers(total_layers[min(total_layers)], "1") * find_numbers(total_layers[min(total_layers)], "2")
+        total_layers.append((find_numbers(individual_layer[:], "0"), individual_layer[:]))
+    return (find_numbers(min(total_layers)[1], "1") * find_numbers(min(total_layers)[1],
+                                                                   "2")), ''.join(
+        decode_message(total_layers, (wide * tall)))
 
 
 def find_numbers(layers, num):
@@ -21,5 +22,25 @@ def find_numbers(layers, num):
     return count
 
 
+def decode_message(layers, total_len):
+    final_layer = []
+    i = 0
+    while i < total_len:
+        color_1 = None
+        for individual_layer in layers:
+            # if color_1 is None:
+            #     color_1 = (individual_layer[1])[i]
+            # else:
+            #     color_1 = find_color_precedence(color_1, (individual_layer[1])[i])
+            if (individual_layer[1])[i] == '0' or (individual_layer[1])[i] == '1':
+                color_1 = (individual_layer[1])[i]
+                break
+            else:
+                color_1 = (individual_layer[1])[i]
+        final_layer.append(color_1)
+        i += 1
+    return final_layer
+
+
 if __name__ == "__main__":
-    print(calculate_bios_password(3, 2, '123456789012'))
+    print(calculate_bios_password(2, 2, '0222112222120000'))
